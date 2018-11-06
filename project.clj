d6b4a4ba7e72853130dad7c273f3bcdf156e31a1
John Schmidt <john.schmidt.h@gmail.com>
2018-11-06 22:04:12 +0100

    Version 0.4.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject java-http-clj "0.3.0"
+(defproject java-http-clj "0.4.0-SNAPSHOT"
   :description "A lightweight Clojure wrapper for java.net.http"
   :url "http://www.github.com/schmee/java-http-clj"
   :license {:name "MIT"
